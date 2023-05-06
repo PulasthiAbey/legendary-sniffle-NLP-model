6e77bbb83316fef61c021eca02e775b258907121
PulasthiAbey <himaruwan2pulasthi@gmail.com>
2023-05-06 13:13:24 +0530

    model updates

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 from flask import Flask, request
-import joblib
+import numpy as np
+import random
 
 app = Flask(__name__)
 
-model = joblib.load('model.pkl')
+history = ["Historiography and Historical Methodologies", "World History and Globalization",
+           "Colonialism and Post-Colonialism", "The Cold War and Its Impact", "The Rise and Fall of Empires", "Comparative Revolutions", "Gender and History", "Environmental History", "War and Society", "Intellectual History"]
+
 
 @app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    prediction = model.predict([data['input']])
-    return {'prediction': int(prediction[0])}
+def predict():   
+    random_history = random.sample(history, 3)
+    print(random_history)
+    return random_history
+
 
 if __name__ == '__main__':
     app.run()
